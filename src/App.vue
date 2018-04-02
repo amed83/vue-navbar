374fb12a0d3255e3cf8ddc68170f66eec4fe063d
unknown <corsiamedeo@gmail.com>
2018-04-02 22:45:37 +0200

    adding images

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,33 @@
+<template>
+            <div>
+                <app-NavBar >
+                </app-NavBar>
+            </div>
+</template>
 
+<script>
+    import NavBar from './components/NavBar.vue'
+    export default {
+        components:{
+            appNavBar:NavBar
+        }
+    }
+</script>
+
+<style >
+    body{
+        margin:0;
+        font-family: "Lato", sans-serif;
+        font-weight: 700;
+        line-height: 1.2;
+        box-sizing: border-box;
+    }
+
+    html{
+    font-size:62.5%;
+    }
+
+</style>
+<style >
+
+</style>
